fe560ce1de1c36ab0e5983c96122c51e8ac1ed39
Rachel Warnock <rachel.warnock@gmail.com>
2024-10-25 14:30:44 +0200

    consolidating simulator functions

--- R/sim.morpho.R
+++ R/sim.morpho.R
@@ -40,3 +40,25 @@ sim.morpho = function(tree, k = 2, nchar = c(10), variable.coding = FALSE, parti
   }
   seq
 }
+
+symmetric.Q.matrix <- function(K) {
+
+  # Create an empty matrix of size K x K
+  Q <- matrix(0, nrow = K, ncol = K)
+
+  # transition rate for each state
+  transition.rate <- 1 / (K)
+
+  for (i in 1:K) {
+    for (j in 1:K) {
+      if (i != j) {
+        Q[i, j] = transition.rate
+      }
+    }
+    Q[i, i] = -(transition.rate*(K-1))
+  }
+
+  # Return the generated transition matrix
+  return(Q)
+}
+
