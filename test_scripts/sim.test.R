ad53900387c906a96f2b3635a3bef3740b7fc3f2
Rachel Warnock <rachel.warnock@gmail.com>
2024-10-25 14:46:00 +0200

    update to morpho file name

--- test_scripts/sim.test.R
+++ test_scripts/sim.test.R
@@ -1,86 +1,52 @@
-sim.test <- function(tree, k = 2, trait.num = 2, ancestral = FALSE){
+sim.morpho.old = function(tree, k = 2, nchar = c(10), variable.coding = FALSE, partitions = 1, alpha = NULL){
+  # Mk
+  # + V
+  # + P
+  # + G
+  # wish list
+  # missing data
+  # FBD range
+  # note k = max state number
 
-  ### for a symmetric Q matrix (default?)
-  Q <- symmetric_Q_matrix(k)
-  trait.num <- trait.num
+  #TODO check tree etc, e.g., tree must tree, tree must root, or we arbitrarily root the tree, check is fully bifurcating
+  #TODO add check partitions and character make sense
+  # if (p > 1 && nchar length == 1, nchar[[1]] / p)
 
-  states <- as.character(c(0:(k-1)))
+  if(is.null(alpha)) rates = 1
+  else rates = phangorn::discrete.gamma(alpha, 4) # TODO: NEED TO CHECK WITH MIKE
 
-  # reorder branches in the phylogeny
-  #tree.ordered <- ape::reorder.phylo(tree, "postorder")
-  tree.ordered <- reorder(tree)
-  edge <- tree.ordered$edge
-  num.nodes <- max(edge)
-  num.tips <- ape::Ntip(tree.ordered)
+  for(p in partitions:1){ # for now, goes backwards from p max. Otherwise we have to deal with attr(seq, "levels"); or you can use cbind(x, seq)
+    if(max(partitions) == 1) states = as.character(c(0:(k-1)))
+    else states = as.character(c(0:p))
 
-  # create an empty matrix
-  output <- matrix(NA, num.nodes, trait.num)
+    for(i in 1:nchar[p]){
+      if(nchar[p] == 0) next
 
-  # define the node labels
-  parent <- as.integer(edge[, 1])
-  child <- as.integer(edge[, 2])
+      x = phangorn::simSeq(tree, l = 1, type = "USER", levels = states, rate = sample(rates, 1))
 
-  # identify the root
-  root <- as.integer(parent[!match(parent, child, 0)][1])
+      if(variable.coding){
+        while( length(unique(as.character(x))) == 1 ){
+          x = phangorn::simSeq(tree, l = 1, type = "USER", levels = states, rate = sample(rates, 1))
+        }
+      }
 
-  # simulate the root state
-  root.state <- sample(states, trait.num, replace = TRUE, prob = rep(1/k, k)) #later prob would need to take into account for bf
-
-  # add the root state to the matrix
-  output[root, ] <- root.state
-
-  # edge lengths
-  bl <- tree.ordered$edge.length
-
-  # loop through all branches
-  for (i in seq_along(bl)){
-
-    from <- parent[i]
-    to <- child[i]
-
-    # generate a matrix of probabilities
-    #P <- getP(tl[i], eig, rate)[[1]]
-    # probability of change along each branch
-    #P  <- lapply(bl[i], function(l) ape::matexpo(Q[[1]] * l))
-    P  <- ape::matexpo(Q * bl[i])
-
-    # avoid numerical problems for larger P and small bl
-    if (any(P < 0)) P[P < 0] <- 0
-
-    for (j in 1:k) {
-      # boolean, if current state is TRUE then we calc the prob of changing from that state. Useful loop is sites > 1, otherwise some redundancy
-      ind <- output[from, ] == states[j]
-      output[to, ind] <- sample(states, sum(ind), replace = TRUE, prob = P[, j])
+      if(i == 1 && p == max(partitions)){
+        seq = x
+      }
+      else seq = cbind(seq, x)
     }
   }
-
-   tip.labels <- c(tree.ordered$tip.label, as.character( (num.tips + 1):num.nodes))
-  rownames(output) <- tip.labels
-
-  # remove ancestral sequences
-  if (!ancestral) output <- output[tree.ordered$tip.label, , drop = FALSE]
-
-  # formatting for morpho object
-   tip_sequences <- rownames(output)
-   sequence = list()
-
-  #  create list of simulated traits
-   for ( i in 1:length(tip_sequences)){
-     sequence[[tip_sequences[i]]] <- output[tip_sequences[i],]
-   }
-
-  # create morpho object
-  sim.output <- as.morpho(sequence, tree.ordered, "Mk")
-
-
-  return(sim.output)
+  seq
 }
+
+
+
 
 
 
 set.seed(123)
 phy <- ape::rtree(10)
 plot(phy)
-simulated_morpho <- sim.test(phy, k = 2, trait.num = 20)
+simulated_morpho <- sim.morpho(phy, k = 2, trait.num = 20)
 simulated_morpho
 
