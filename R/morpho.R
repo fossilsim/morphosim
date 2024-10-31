4da80910b5e9b1404674271d2f7acf61d0f1012f
Rachel Warnock <rachel.warnock@gmail.com>
2024-10-31 11:31:08 +0100

    adding documention, reorganising some functions

--- R/morpho.R
+++ R/morpho.R
@@ -1,51 +1,58 @@
-# Define the morpho constructor
-morpho <- function(data, tree, model) {
+#' Mopho object
+#'
+#' Create a morpho.
+#'
+#' @param data ?
+#' @param tree ?
+#' @param tree Tree with branches that represent genetic distance associated with the character data.
+#' @param time.tree Tree with branches that represent time associated with the character data.
+#'
+#' @export
+morpho <- function(data = NULL, tree = NULL, time.tree = NULL, model = NULL){
+
+  # check the number of sequences match the number of tips in the tree
+  if (length(data) != length(tree$tip.label)) {
+    stop("Number of sequences doesn't match the number of tips in the tree.")
+  }
+
+  # check each sequence contains the same number of characters
+  if (length(unique(sapply(data, length))) != 1) {
+    stop("Each sequence should contain the same number of characters.")
+  }
 
   # Create a list to store sequences, tree, and model
-  morpho_list <- list(
+  morpho.list <- list(
     sequences = data,
     tree = tree,
     model = model
   )
 
-  ## Checks
-  # 1. Does the number of sequences match the number of tips in the tree
-  if (length(data) != length(tree$tip.label)) {
-    stop("Number of sequences doesn't match the number of tips in the tree.")
-  }
-
-  # 2. Does each sequence contain the same number of characters?
-  if (length(unique(sapply(data, length))) != 1) {
-    stop("Each sequence should contain the same number of characters.")
-  }
-
   # Assign class "morpho" to the object
   #attr(morpho, "class") <- c("morphology", class(morpho))
-  class(morpho_list) <- "morpho"
-  return(morpho_list)
+  class(morpho.list) <- "morpho"
+  return(morpho.list)
 }
 
-# Define print method for the morpho object
-### this was copied from fossil sim so might want to change it up at some stage!!
-# Define print method for the morpho object
-# probably don't need to print the whole alignmnet here
-print.morpho <- function(x, max.length = 5, ...) {
+#' @export
+#' @aliases morpho
+print.morpho <- function(x, max.length = 5, ...){
+
   # Convert sequences into a data frame
-  seq_data <- t(as.data.frame(x$sequences))
+  seq.data <- t(as.data.frame(x$sequences))
 
-  # Print first `max.length` rows (or fewer if max.length exceeds the number of rows)
-  #print(head(seq_data, max.length))
-
-  print(seq_data[,1:max.length])
+  print(seq.data[,1:max.length])
 
   # Print a summary of the morphological data
   cat("Morphological data for", length(x$sequences), "taxa with",
-      length(x$sequences[[1]]), "traits per taxon and", sort(unique(c(seq_data[,1], seq_data[,2]))),
+      length(x$sequences[[1]]), "traits per taxon and", sort(unique(c(seq.data[,1], seq.data[,2]))),
       "as character states\n")
       cat("Showing", max.length, "traits here for now\n")
 }
-# Define summary method for the morpho object
-summary.morpho <- function(object, max.length = 5, ...) {
+
+#' @export
+#' @aliases morpho
+summary.morpho <- function(object, max.length = 5, ...){
+
   # Use print.morpho to show the data
   print(object, max.length = max.length)
 
@@ -54,17 +61,19 @@ summary.morpho <- function(object, max.length = 5, ...) {
   cat("Model:", object$model, "\n")
 }
 
-# Define the as.morpho generic function
+#' @export
+#' @rdname morpho
 as.morpho <- function(data, tree, model) {
   UseMethod("as.morpho")
 }
 
-# Default method for as.morpho
-as.morpho.default <- function(data, tree, model) {
-  morpho(data, tree, model)
+#' @export
+as.morpho.default <- function(data, ...) {
+  morpho(data, ...)
 }
 
-# Define is.morpho function to check if the object is of class 'morpho'
+#' @export
+#' @rdname morpho
 is.morpho <- function(data) {
   inherits(data, "morpho")
 }
