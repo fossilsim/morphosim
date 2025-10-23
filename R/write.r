e9e40b1e52e05a792cdb26b5f5964117577fd9b6
laumul <lauramulvey479@gmail.com>
2025-10-23 10:31:09 +0100

    added examples to writing functions

--- R/write.r
+++ R/write.r
@@ -6,10 +6,14 @@
 #' @param data Morpho object
 #' @param file File name
 #'
+#' @examples
+#' \dontrun{
+#' write.recon.tree(data = morpho_data, file = "reconstructed.tre")
+#' }
 #' @export
 #'
 write.recon.tree <- function (data, file) {
-  
+
   r_tree <- FossilSim::reconstructed.tree.fossils.objects(fossils  = data$fossil,
                                                           tree = data$trees$TimeTree)
   ape::write.tree(r_tree$tree, file = file)
@@ -24,11 +28,14 @@ write.recon.tree <- function (data, file) {
 #' @param file File name
 #' @param keep_matrix Logical. If TRUE, returns a matrix showing the naming transformations
 #' between `morphosim` and `fossilsim` of Sampled ancestors.
-#'
+#' @examples
+#' \dontrun{
+#' write.recon.matrix(data = morpho_data, file = "reconstructed.nex")
+#'}
 #' @export
 #'
 write.recon.matrix <- function (data, file = NULL, keep_matrix = F) {
-  
+
   r_tree <- FossilSim::reconstructed.tree.fossils.objects(fossils  = data$fossil,
                                                           tree = data$trees$TimeTree)
   SA_tips <- c()
@@ -36,50 +43,50 @@ write.recon.matrix <- function (data, file = NULL, keep_matrix = F) {
   matches <- grepl("_1$", tps )
   # Extract elements that match
   reconTreeTips <- gsub("_1$", "", tps[matches])
-  
+
   ## add these tip labels to the file + plus all sampled ancestor
   seq_tips <- which(names(data$sequences$tips) %in% reconTreeTips)
-  
+
   matches <- grepl("_2$", tps )
   reconSA <-  gsub("_2$", "", tps[matches])
-  
+
   transformation <- matrix(ncol = 2, nrow = length(reconSA))
   colnames(transformation) <- c("Morphosim", "Fossilsim")
   if (length(reconSA) > 0){
-    
+
     for (l in 1:length(reconSA)){
       t_label <- which(data$trees$TimeTree$tip.label == reconSA[l])
       b_num <- which(data$trees$TimeTree$edge[,2] == t_label)
       spec_min <- min(data$fossil$hmin[data$fossil$ape.branch == b_num])
       spec_num <- data$fossil$specimen[ data$fossil$hmin == spec_min ]
       SA_tips <- rbind(SA_tips, c(paste0(spec_num, "_", b_num)))
-      
+
       transformation[l,"Morphosim"] <- SA_tips[l]
       transformation[l,"Fossilsim"] <- reconSA[l]
     }
-    
+
     total_tips <- c(data$sequences$tips[c(seq_tips)], data$sequences$SA[c(SA_tips)])
   } else {
     total_tips <- data$sequences$tips[c(seq_tips)]
   }
-  
+
   ## need to change the sequence names to match the reconstructed tree
   for ( l in 1:length(seq_tips)){
     current <- names(data$sequences$tips[c(seq_tips)[l]])
     names(total_tips)[names(total_tips) == current ] <- paste0(current, "_1")
   }
-  
+
   if (length(reconSA) > 0){
     for( l in 1:length(SA_tips)){
       rematch <- unname(transformation[l,"Fossilsim"])
       names(total_tips)[names(total_tips) == SA_tips[l]] <- paste0(rematch, "_2")
     }
   }
-  
+
   if(!is.null(file)) {
     ape::write.nexus.data(total_tips , file = file)
   }
-  
+
   if(keep_matrix){
     return(transformation)
   }
@@ -98,17 +105,19 @@ write.recon.matrix <- function (data, file = NULL, keep_matrix = F) {
 #'  The ages in the object are point estimates by default; setting `uncertainty`
 #'  will create an age range of ± this value (in millions of years).
 #'
-#'
+#' @examples
+#' \dontrun{
+#' write.tsv(data = morpho_data, file = "ages.tsv")
+#'}
 #' @export
-
 write.tsv <- function (data, file, uncertainty = 0) {
-  
+
   ## ages of full tree
   tip_depths <- ape::node.depth.edgelength(data$trees$TimeTree)[1:length(data$trees$TimeTree$tip.label)]
   tree_height <- max(ape::node.depth.edgelength(data$trees$TimeTree))
   tip_ages <-   round(abs(tip_depths - tree_height),3)
   # extant_tips <- data$trees$TimeTree$tip.label[abs(tip_depths - tree_height) < 1e-8]
-  
+
   cat("taxon", "min_age", "max_age", sep = "\t", "\n", file = file)
   for ( i in 1:length(tip_ages)){
     if (tip_ages[i] == 0){
@@ -134,32 +143,37 @@ write.tsv <- function (data, file, uncertainty = 0) {
 #' @param uncertainty Numeric. Adds uncertainty to fossil ages in the morpho object.
 #'  The ages in the object are point estimates by default; setting `uncertainty`
 #'  will create an age range of ± this value (in millions of years).
+#'  #' @examples
+#' \dontrun{
+#' write.recon.tsv(data = morpho_data, file = "recon_ages.tsv")
+#'}
+#' @export
 
 write.recon.tsv <- function (data, file, uncertainty = 0){
-  
+
   r_tree <- FossilSim::reconstructed.tree.fossils.objects(fossils  = data$fossil,
                                                           tree = data$trees$TimeTree)
   transformations <- write.recon.matrix(data, keep_matrix = T)
-  
+
   cat("taxon", "min_age", "max_age", sep = "\t", "\n", file = file)
-  
-  
+
+
   ## true tree tips
   tps <- unname(r_tree$tree$tip.label)
   matches <- grepl("_1$", tps )
   # Extract elements that match
   reconTreeTips <- gsub("_1$", "", tps[matches])
-  
+
   ## add these tip labels to the file + plus all sampled ancestor
   seq_tips <- which(names(data$sequences$tips) %in% reconTreeTips)
-  
-  
+
+
   for ( i in 1:length(reconTreeTips)){
     ord <-  which(data$trees$TimeTree$tip.label ==reconTreeTips[i])
     node_pos <- ape::node.depth.edgelength(data$trees$TimeTree)[ord]
     tree_height <- max(ape::node.depth.edgelength(data$trees$TimeTree))
     tip_ages <-   round(abs(node_pos - tree_height),3)
-    
+
     if(tip_ages == 0){
       nm <- paste0(reconTreeTips[i], "_1")
       cat(nm, tip_ages, tip_ages,
@@ -175,23 +189,23 @@ write.recon.tsv <- function (data, file, uncertainty = 0){
       cat(nm,min_age, (tip_ages + uncertainty),
           sep = "\t", file = file, append = T )
       cat("\n", file = file, append = TRUE)
-      
+
     }
   }
-  
+
   ## sampled ancestors
-  
+
   if (length(transformations[,"Morphosim"]) > 0){
     for (i in 1:length(transformations[,1])){
-      
+
       parts <- as.numeric(strsplit(transformations[i, "Morphosim"], "_")[[1]])
       specimen_num <- parts[1]
       branch_num   <- parts[2]
-      
+
       # Subset the data frame to get hmin
       hmin <- data$fossil$hmin[data$fossil$ape.branch == branch_num &
                                  data$fossil$specimen  == specimen_num]
-      
+
       nm <- paste0(transformations[i, "Fossilsim"], "_2")
       if (hmin - uncertainty < 0){
         min_age <- 0
